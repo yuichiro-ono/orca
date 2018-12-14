716f1d652aff1f781d65d71192983659b25cb486
Yuichiro Ono <u1ax.ono@gmail.com>
2018-12-14 23:39:34 +0900

    2018/12/14

--- var/www/cgi-bin/constantvals.rb
+++ var/www/cgi-bin/constantvals.rb
@@ -111,7 +111,7 @@ module ConstantValues
             currentOrdNo = lastOrdNo.nil? ? 1 : lastOrdNo + 1
 
             newPatientIDs.each do |id|
-            	puts id
+            	puts "empty" if id ==""
                 DB.exec("UPDATE t_orca_reception SET order_no = #{currentOrdNo} WHERE patient_id == \'#{id}\';")
                 currentOrdNo += 1
             end
