0ac1cf20e127c23746c5be1a8cc48a27458bdb8f
Yuichiro Ono <u1ax.ono@gmail.com>
2018-12-14 23:55:40 +0900

    2018/12/14

--- var/www/cgi-bin/constantvals.rb
+++ var/www/cgi-bin/constantvals.rb
@@ -117,7 +117,9 @@ module ConstantValues
             end
 
             # T_RECEPTIONに新規受付情報を追加
-            DB.exec("INSERT INTO t_reception SELECT * FROM t_orca_reception WHERE t_orca_reception.patient_id IN (\'#{newPatientIDs[0].join("\', \'")}\");")
+            newPatientIDs.each do |id| 
+	            DB.exec("INSERT INTO t_reception SELECT * FROM t_orca_reception WHERE (t_orca_reception.patient_id = \'#{id}\');")
+	      	end
         end
 
         # 受付取り消し患者のT_RECEPTIONからの削除
