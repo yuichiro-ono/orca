40a7b7543787de2b57729d1006be858f52524131
Yuichiro Ono <u1ax.ono@gmail.com>
2018-12-16 18:58:17 +0900

    2018/12/16

--- home/orcauser/scripts/printacceptnum/printToThermprt.py
+++ home/orcauser/scripts/printacceptnum/printToThermprt.py
@@ -14,8 +14,8 @@ accept_date = args[2]
 accept_time = args[3]
 
 acceptDatetime = datetime.strptime(accept_date + ' ' + accept_time, '%Y-%m-%d %H:%M:%S')
-acceptDatetime_outtext = hex(acceptDatetime.year) + '944e' + hex(acceptDatetime.month) + '8c8e' + hex(acceptDatetime.day) + \
-	'93fa8169' + WEEKDAY[acceptDatetime.weekday] + '816a20' + hex(acceptDatetime.hour) + '8e9e' + hex(acceptDatetime.minute) + '95aa'
+acceptDatetime_outtext = hex(acceptDatetime.year()) + '944e' + hex(acceptDatetime.month()) + '8c8e' + hex(acceptDatetime.day()) + \
+	'93fa8169' + WEEKDAY[acceptDatetime.weekday()] + '816a20' + hex(acceptDatetime.hour()) + '8e9e' + hex(acceptDatetime.minute()) + '95aa'
 
 Seiko = printer.Network(PRINTER_IP)
 Seiko._raw("1BH 52H 8")		# International characer select
