96a4644125d28ff6c942670f52f4f1505c793255
Yuichiro Ono <u1ax.ono@gmail.com>
2018-12-16 19:33:10 +0900

    2018/12/16

--- home/orcauser/scripts/printacceptnum/printToThermprt.py
+++ home/orcauser/scripts/printacceptnum/printToThermprt.py
@@ -19,9 +19,10 @@ print acceptDatetime_outtext
 
 Seiko = Network(PRINTER_IP)
 
+Seiko._raw("1BH 40H")
 Seiko._raw("1BH 52H 8")		# International characer select
 Seiko._raw("1CH 43H 1")	    # FS C （Shift JISコード体系を選択する）電源offまで有効
-Seiko.text("8ef3957493fa8e9e8146H" + acceptDatetime_outtext)
+#Seiko.text("8ef3957493fa8e9e8146H" + acceptDatetime_outtext)
 '''
 Seiko.set("center")
 Seiko.text("8ef3957494d48d86H")
@@ -31,5 +32,5 @@ Seiko.text(accept_ID)
 Seiko.qr("http://ashiyaekimaeclinic.aaa.com/wait/" + accept_ID)
 #Seiko.ln(1)
 Seiko.text("81aa8fe382cc51528352815b836882f093c782dd8d9e82de82c6908492e891d282bf8e9e8ad482aa955c8ea682b382ea82dc82b781aaH")
-'''
 Seiko.cut()
+'''
