496146941c07efac5ea4609795d66fddfbed8eb9
Yuichiro Ono <u1ax.ono@gmail.com>
2018-12-16 22:05:45 +0900

    2018/12/16

--- home/orcauser/scripts/printacceptnum/printToThermprt.py
+++ home/orcauser/scripts/printacceptnum/printToThermprt.py
@@ -6,7 +6,7 @@ import sys
 from datetime import datetime
 
 def jpInit(printer):
-    printer.charcode('S-JIS')
+    printer.charcode('JIS')
     printer._raw(b'\x1c\x43\x01')	# Kanji Code System Selection: FS 'C'
 
 def jpText(printer, txt, dw=False, dh=False):
