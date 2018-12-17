1fbee05d22d18d9fe121dd36aebc5729014a89e0
Yuichiro Ono <u1ax.ono@gmail.com>
2018-12-17 22:10:46 +0900

    2018/12/17

--- home/orcauser/scripts/printacceptnum/printToThermprt.py
+++ home/orcauser/scripts/printacceptnum/printToThermprt.py
@@ -52,95 +52,6 @@ def jpText(printer, txt, dw=False, dh=False):
 		printer._raw(b'\x1c\x21\x00') # Char size OFF
 	printer._raw(b'\x1c\x2e')         # Kanji mode OFF
 
-def qr(printer, text):
-	""" Print QR Code for the provided string """
-	qr_code = qrcode.QRCode(version=4, box_size=4, border=1)
-	qr_code.add_data(text)
-	qr_code.make(fit=True)
-	qr_img = qr_code.make_image()
-	im = qr_img._img.convert("RGB")
-
-    # Convert the RGB image in printable image
-	_convert_image(printer, im)
-
-def _print_image(printer, line, size):
-	""" Print formatted image """
-	i = 0
-	cont = 0
-	buffer = ""
-
-	printer._raw(S_RASTER_N)
-	buffer = "%02X%02X%02X%02X" % (((size[0]/size[1])/8), 0, size[1], 0)
-	printer._raw(binascii.unhexlify(buffer))
-	buffer = ""
-
-	while i < len(line):
-		hex_string = int(line[i:i+8],2)
-		buffer += "%02X" % hex_string
-		i += 8
-		cont += 1
-		if cont % 4 == 0:
-			printer._raw(binascii.unhexlify(buffer))
-			buffer = ""
-	cont = 0
-
-def _check_image_size(size):
-	""" Check and fix the size of the image to 32 bits """
-	if size % 32 == 0:
-		return (0, 0)
-	else:
-		image_border = 32 - (size % 32)
-		if (image_border % 2) == 0:
-			return (image_border / 2, image_border / 2)
-		else:
-			return (image_border / 2, (image_border / 2) + 1)
-
-def _convert_image(printer, im):
-	""" Parse image and prepare it to a printable format """
-	pixels   = []
-	pix_line = ""
-	im_left  = ""
-	im_right = ""
-	switch   = 0
-	img_size = [ 0, 0 ]
-
-
-	if im.size[0] > 512:
-		print  ("WARNING: Image is wider than 512 and could be truncated at print time ")
-	if im.size[1] > 255:
-		raise ImageSizeError()
-
-	im_border = _check_image_size(im.size[0])
-	for i in range(int(im_border[0])):
-		im_left += "0"
-	for i in range(int(im_border[1])):
-		im_right += "0"
-
-	for y in range(im.size[1]):
-		img_size[1] += 1
-		pix_line += im_left
-		img_size[0] += im_border[0]
-		for x in range(im.size[0]):
-			img_size[0] += 1
-			RGB = im.getpixel((x, y))
-			im_color = (RGB[0] + RGB[1] + RGB[2])
-			im_pattern = "1X0"
-			pattern_len = len(im_pattern)
-			switch = (switch - 1 ) * (-1)
-			for x in range(pattern_len):
-				if im_color <= (255 * 3 / pattern_len * (x+1)):
-					if im_pattern[x] == "X":
-						pix_line += "%d" % switch
-					else:
-						pix_line += im_pattern[x]
-					break
-				elif im_color > (255 * 3 / pattern_len * pattern_len) and im_color <= (255 * 3):
-					pix_line += im_pattern[-1]
-					break
-		pix_line += im_right
-		img_size[0] += im_border[1]
-
-	_print_image(printer, pix_line, img_size)
 
 WEEKDAY = ['月', '火', '水', '木', '金', '土', '日']
 PRINTER_IP = "192.168.0.17"
@@ -159,14 +70,12 @@ Seiko = Network(PRINTER_IP)
 
 jpInit(Seiko)
 jpText(Seiko, '受付日時：' + acceptDatetime_outtext + '\n\n')
-Seiko.set(align="center")
-jpText(Seiko, '受付番号：')
+jpText(Seiko, '受付番号：' + accept_ID + '\n', dw=True, dh=True)
 #Seiko.set(align="center", bold=True, double_width=True, double_height=True)
-Seiko.text(accept_ID + '\n')
 #Seiko.ln(2)
 #Seiko._raw(b'\x1b\x52\x00')
 Seiko.qr("http://ashiyaekimaeclinic.aaa.com/wait/" + accept_ID, size=4, center=True)
-jpText(Seiko, 'おしまい')
+jpText(Seiko, '↑のQRコードを読み込むと，待ち時間が表示されます')
 Seiko.cut()
 
 
