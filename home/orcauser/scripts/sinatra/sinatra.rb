a85210c6d3edd1d1aafcf40015e493a5858f7861
Yuichiro Ono <u1ax.ono@gmail.com>
2018-12-18 01:01:08 +0900

    2018/12/18

--- home/orcauser/scripts/sinatra/sinatra.rb
+++ home/orcauser/scripts/sinatra/sinatra.rb
@@ -54,7 +54,7 @@ end
 
 put '/reissue' do 
 	"Hello World!"
-	system("python /home/orcauser/scripts/printacceptnum/printToThermprt.py #{params['acceptid']} #{params['acceptdate']} #{params['accepttime']}")
+#	system("python /home/orcauser/scripts/printacceptnum/printToThermprt.py #{params['acceptid']} #{params['acceptdate']} #{params['accepttime']}")
 end
 
 get '/' do
