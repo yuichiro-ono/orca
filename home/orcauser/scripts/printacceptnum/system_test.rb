fc3c1f56b3f0f47fdc613c6e52145ee702830983
Yuichiro Ono <u1ax.ono@gmail.com>
2018-12-18 22:26:17 +0900

    2018/12/18

--- home/orcauser/scripts/printacceptnum/system_test.rb
+++ home/orcauser/scripts/printacceptnum/system_test.rb
@@ -1,2 +1,3 @@
 system("cd /home/orcauser/scripts/printtacceptnum")
-system("python printToThermprt.py 9999 2018-12-19 00:00:00")
+system("python aa.py")
+#system("python printToThermprt.py 9999 2018-12-19 00:00:00")
