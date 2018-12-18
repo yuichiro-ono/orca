6f2cce7d5d7c389d16cbccb18b9d7f38edf06846
Yuichiro Ono <u1ax.ono@gmail.com>
2018-12-19 00:54:49 +0900

    2018/12/19

--- home/orcauser/scripts/makereceptionlist/make_reception_list.rb
+++ home/orcauser/scripts/makereceptionlist/make_reception_list.rb
@@ -1,14 +1,18 @@
 #!/usr/bin/ruby
 
 require '/var/www/cgi-bin/constantvals'
+require 'active_support/time'
 require 'clockwork'
+require 'logger'
 
 include Clockwork
 include ConstantValues
 
+@logger = Logger.new("make_reception_list.log")
 
 ## 1分毎に新しい予約メールとキャンセルメールを確認する　##
 every(1.minute, 'check_mail.job') {
   completeDocument = combineWithPhonenumber(getReceptionXML)
   writeReceptionListToDb(completeDocument)
+  @logger.info("Re-genareted reception list")
 }
