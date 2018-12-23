91e1f3ea87f7206bb24f82972a4161b3c5b4e854
Yuichiro Ono <u1ax.ono@gmail.com>
2018-12-24 00:14:26 +0900

    2018/12/24

--- home/orcauser/scripts/sinatra/test.rb
+++ home/orcauser/scripts/sinatra/test.rb
@@ -8,7 +8,7 @@ TIME_FOR_ONE_PATIENT = 7
 DB_HOST = 'ec2-54-163-245-64.compute-1.amazonaws.com'.freeze
 DB_NAME = 'd6190mj636qia0'.freeze
 DB_USER = 'ikezymvynmeniz'.freeze
-DB_PASSWD = '63078f2a-887d-4699-920f-e51636e399d8'.freeze
+DB_PASSWD = '669297b5ee9de1d2e7379f50cde455105447d14718b3a538510da4230caa28e7'.freeze
 DB = PG::Connection.new(:host => DB_HOST, :dbname => DB_NAME, :user => DB_USER, :password => DB_PASSWD, :sslmode => 'require')
 DB.internal_encoding = "UTF-8"
 
