cd2e5c29ba13ce8e3fa30c4b6aac986f7f51396e
Yuichiro Ono <u1ax.ono@gmail.com>
2018-12-24 12:53:01 +0900

    2018/12/24

--- home/orcauser/scripts/exportheroku/export_heroku.rb
+++ home/orcauser/scripts/exportheroku/export_heroku.rb
@@ -11,5 +11,6 @@ logger = Logger.new("export_heroku.log")
 ## 5分毎にheroku PostgreSQLサーバーに受付リストを送信する　##
 every(5.minutes, 'export_reception_list_to_heroku.job') {
   exportDataToHeroku
+  logger.info("Sent reception date to heroku!")
 }
 
