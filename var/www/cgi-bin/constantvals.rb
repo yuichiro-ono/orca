1fb6d2986d87bfdd395a88b45d98cdf449d3b4d9
Yuichiro Ono <u1ax.ono@gmail.com>
2019-04-16 17:48:43 +0900

    2019/04/16

--- var/www/cgi-bin/constantvals.rb
+++ var/www/cgi-bin/constantvals.rb
@@ -17,9 +17,9 @@ module ConstantValues
 	# Test URL
 	ORCA_URI = 'http://localhost:8000/'.freeze
 	RECEPTIONLIST_CGI_URI = '/cgi-bin/receptionlist.cgi'
-	DBUPDATE_ORDERNO_URI = 'http://localhost:4567/ordno'
-	DBUPDATE_WAITINGSTATUS_URI = 'http://localhost:4567/wtst'
-    DBUPDATE_REISSUE_URI = 'http://localhost:4567/reissue'
+	DBUPDATE_ORDERNO_URI = 'http://192.168.0.81:4567/ordno'
+	DBUPDATE_WAITINGSTATUS_URI = 'http://192.168.0.81:4567/wtst'
+    DBUPDATE_REISSUE_URI = 'http://192.168.0.81:4567/reissue'
 
 	# XML Formatter
 	Formatter = REXML::Formatters::Default.new
